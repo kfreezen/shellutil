ba45021a24a625f5fc8d1581ac547fd20f10ff67
Kent Friesen <kfreezen@gmail.com>
2021-02-11 15:39:35 -0500

    remove print

--- abstractshell/id.py
+++ abstractshell/id.py
@@ -7,8 +7,6 @@ _id_parse = re.compile(rf"uid\={_id_re}\s+gid\={_id_re}\s+groups=(.*)")
 
 
 def id_parse(out):
-    print(_id_parse)
-
     match = _id_parse.match(out)
     if not match:
         return None
